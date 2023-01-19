a31c299602d048a83f89fcb91b090eb01f19fe0a
Shamon <shamonp50@gmail.com>
2023-01-19 19:34:21 +0530

    Updated cors variables

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.use('/assets',express.static("assets"))
 //FILE STORAGE
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "/assets")
+        cb(null, "public/assets")
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
