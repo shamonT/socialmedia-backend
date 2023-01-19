443bfe1c862a59e4c786c517dd8b86a06b908491
Shamon <shamonp50@gmail.com>
2023-01-19 18:01:48 +0530

    image env bugs git issue

--- index.js
+++ index.js
@@ -97,9 +97,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static("public"));
+// app.use("/images", express.static("./public/assets"));
 app.use(
   "/assets",
-  express.static(path.join(__dirname, "public/assets"))
+  express.static(path.join(__dirname, "./public/assets"))
 );
 
 // filestorage//
